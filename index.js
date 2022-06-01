43916c4716072ddb7d9d27623bda00ee39eff521
Sylvester <sylvestertrevi@gmail.com>
2022-06-01 00:34:16 -0400

    commit tree fe46a4dc8a60d22e1bf52c398edad10874a51872

--- index.js
+++ index.js
@@ -1,2 +1,12 @@
-
 // Write your code here
+let num1 = 2
+let num2 = 31;
+let multiply = num1 * num2;
+ 
+let random = Math.floor(Math.random() * 10) + 2;
+
+let num3 = 24;
+let num4 = 5;
+let mod = num3 % num4;
+
+let max = Math.max(20, 2.7,3, -1.2, 19, 7);
